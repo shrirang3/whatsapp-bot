e1545e47274a1800aed7903f0556d007a57b4704
Shrirang Mahankaliwar <126176376+shrirang3@users.noreply.github.com>
2025-09-21 11:45:22 +0530

    Update conversation_handler_api.py

--- conversation_handler_api.py
+++ conversation_handler_api.py
@@ -12,9 +12,9 @@ import csv
 # -------------------------
 # CONFIG
 # -------------------------
-CHAT_API_URL = "https://8795a39e8611.ngrok-free.app/chat"   # Your RAG bot API
-WATI_API_BASE = "https://live-mt-server.wati.io/200128/api/v1"   # Your WATI base
-WATI_TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJhMTc3MzU3Mi01ODFiLTQ4MzQtOWJiOS1mYWM2NjRkYzNiMjAiLCJ1bmlxdWVfbmFtZSI6ImVrYWdhckB2YWhhbi5jbyIsIm5hbWVpZCI6ImVrYWdhckB2YWhhbi5jbyIsImVtYWlsIjoiZWthZ2FyQHZhaGFuLmNvIiwiYXV0aF90aW1lIjoiMTAvMDUvMjAyMyAwNzozNjo1NyIsImRiX25hbWUiOiJtdC1wcm9kLVRlbmFudHMiLCJ0ZW5hbnRfaWQiOiIyMDAxMjgiLCJodHRwOi8vc2NoZW1hcy5taWNyb3NvZnQuY29tL3dzLzIwMDgvMDYvaWRlbnRpdHkvY2xhaW1zL3JvbGUiOiJBRE1JTklTVFJBVE9SIiwiZXhwIjoyNTM0MDIzMDA4MDAsImlzcyI6IkNsYXJlX0FJIiwiYXVkIjoiQ2xhcmVfQUkifQ.ud5AYN5Kf5Xm17WX8Krr1Z3qJo0waJu75cY-o3N2EuU"
+CHAT_API_URL = "bot_api_url"   # Your RAG bot API
+WATI_API_BASE = "wati_base"   # Your WATI base
+WATI_TOKEN = "wati_token"
   # Make sure this is your complete token
 
 
